710a65b41d9826b5317c6818fd238e424fbecc80
angrieta <46126691+angrieta@users.noreply.github.com>
2025-02-07 01:34:54 +0900

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,30 @@
 var bnr = new Swiper(".mainBnr", {
+    autoplay:{
+        delay:2000, //다음 슬라이드전환까지 대기시간
+        disableOnInteraction:false, // 사용자 상호작용 상관없이 계속 진행
+    },
+    loop:true,
     slidesPerView: 3,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var sale = new Swiper(".saleContent", {
+    loop:true,
+    slidesPerView: 6,
+    spaceBetween: 12,
     pagination: {
     el: ".swiper-pagination",
     clickable: true,
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
-var sale = new Swiper(".saleContent", {
+var sale = new Swiper(".newContent", {
+    loop:true,
     slidesPerView: 6,
     spaceBetween: 12,
     pagination: {
